f0b14b5e4a29dcf2f52b27216b1e0c8bf04bd401
alexkaasik <aleks.kaasik@outlook.com>
2024-02-21 20:52:49 +0200

    update to gui file

--- gui.py
+++ gui.py
@@ -4,54 +4,48 @@ from wordle import *
 word1 = wordR()
 print(word1)
 
-gui = Tk()
-gui.geometry('250x300')
-
 ent = []
+test1 = []
+
 a = int(0)
 go = int(0)
-test1 = [] 
-StopPoint = 0
+StopPoint = int(0)
+
+gui = Tk()
+gui.geometry('250x300')
 
 def kep_pressed(event):
     global a
     global go
     global StopPoint
-    if (event.char == '\x08'):
-        if a > StopPoint:
-            a-=1
-            ent[a]['text']=''
+    global test1
 
-    elif (go == 0):
-        if not (event.char == '\r' or event.char == ' ' or len(event.keysym)>2):
-            ent[a]['text']=event.char.lower()
-            a+=1
-
-    if ( a % 5 ) <= 5:
-        go = 0
-
-    if a % 5 == 0 and a != 0:
-        go = 1
-
-    if go == 1 and event.char == '\r':
-        global test1
+    if ( event.char == '\x08' ) and ( a > StopPoint ):
+        a-=1
+        ent[a]['text']=''
+        if a%5 < 5:
+            go = 0
+    elif ( event.char == '\r' ):
         go = 0
 
         test1 = [ 
-                ent[a-5]['text'],
-                ent[a-4]['text'],
-                ent[a-3]['text'],
-                ent[a-2]['text'],
-                ent[a-1]['text']
-                ]
+            ent[a-5]['text'],
+            ent[a-4]['text'],
+            ent[a-3]['text'],
+            ent[a-2]['text'],
+            ent[a-1]['text']
+        ]
         test1 = "".join(test1)
-        print(f"count1:{a}")
-        print(f"count2:{StopPoint}")
-        f = open("wordlist.txt","r", encoding="utf-8-sig")
-        if f"{test1}\n" in f.readlines():
-            check(test1)
-        f.close()      
-
+        check(test1)
+        #f = open("wordlist.txt","r", encoding="utf-8-sig")
+        #if f"{test1}\n" in f.readlines():
+            
+        #f.close()      
+    elif (not ( event.char == ' ' or len(event.keysym)>2 )) and ( go == 0 ):
+        ent[a]['text']=event.char.lower()
+        a+=1
+        if a%5 == 0 and a != 0:
+            go = 1
 def check(word2):
     count1 = 0
     global StopPoint
@@ -68,8 +62,8 @@ def check(word2):
         
         ent[a-(5-count1)]['bg'] = color
         count1 +=1
-    if word1 == word2:
-        exit()
+    #if word1 == word2:
+    #    exit()
     
 for i in range(30):
     ent.append(Label(gui,text = ' ',bg="white",font="Arial 24",width=1, borderwidth=4, relief="solid"))
